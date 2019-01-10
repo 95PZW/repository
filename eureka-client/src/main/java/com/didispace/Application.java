1cb6bf60220b2bb81ba58064beacd9499f5ed8f5
95PZW <1805257239@qq.com>
2019-01-10 16:54:03 +0800

    Create service visitors

---
+++ eureka-client/src/main/java/com/didispace/Application.java
@@ -0,0 +1,12 @@
+package com.didispace;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
+
---
+++ eureka-client/src/main/java/com/didispace/DcController.java
@@ -0,0 +1,16 @@
+package com.didispace;
+
+@RestController
+public class DcController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+
+}
