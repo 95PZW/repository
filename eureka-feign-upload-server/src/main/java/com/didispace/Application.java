2be6be78fbc83996ea322d7a2f487c40268987b4
95PZW <1805257239@qq.com>
2019-01-11 16:23:43 +0800

    a new eureak-feign-upload-server service provider is created

---
+++ eureka-feign-upload-server/src/main/java/com/didispace/Application.java
@@ -0,0 +1,29 @@
+package com.didispace;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {/*服务提供方(接收文件)按Spring MVC的正常实现方式即可*/
+
+    @RestController
+    public class UploadController {
+        @PostMapping(value = "/uploadFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+        public String handleFileUpload(@RequestPart(value = "file") MultipartFile file) {
+            return file.getName();
+        }
+    }
+    public static void main(String[] args) {
+       new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
+
