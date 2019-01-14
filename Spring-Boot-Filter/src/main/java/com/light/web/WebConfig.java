84417248112a28eb338be9c64472611c8ebe0fc6
95PZW <1805257239@qq.com>
2019-01-14 16:48:29 +0800

    a new Spring-Boot-Filer project was created

---
+++ Spring-Boot-Filter/src/main/java/com/light/web/WebConfig.java
@@ -0,0 +1,33 @@
+package com.light.web;
+
+import com.light.filterTest.TimeFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class WebConfig {
+
+   @Bean
+   public ServletRegistrationBean servletRegistrationBean() {
+      return new ServletRegistrationBean(new ServletTest(),"/servletTest");
+   }
+
+   @Bean
+    public FilterRegistrationBean timeFilter(){
+       FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+
+       TimeFilter timeFilter = new TimeFilter();
+       registrationBean.setFilter(timeFilter);
+
+       List<String> urls = new ArrayList<>();
+       urls.add("/*");
+       registrationBean.setUrlPatterns(urls);
+
+       return registrationBean;
+   }
+
+}
